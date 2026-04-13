cc362d12c77cc565bf4f67ef79ed56a5f8683b91
Katsuyuki Omuro <komuro@fastly.com>
2026-04-13 14:09:57 +0900

    Write event handler as async function

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,20 @@ import { app } from "./app.js";
 /**
     Attach a FetchEventListener, this is the entry point to all Fastly Compute JavaScript applications
  */
-addEventListener("fetch", (event) => {
-    event.respondWith(
-        Promise.resolve(event)
-            .then(app)
-            .catch(error => {
-                console.error(error.message);
-                console.error(error.stack);
-                return new Response('Internal Server Error', { status: 500 });
-            })
-    )
-})
+addEventListener("fetch", (event) => event.respondWith(handleEvent(event)));
+
+async function handleEvent(event: FetchEvent) {
+
+  let response: Response;
+  try {
+    response = await app(event);
+  } catch(error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      console.error(error.stack);
+    }
+    return new Response("Internal Server Error", {status: 500});
+  }
+  return response;
+
+}
